487a20a4ec67e791872644fbdbd8f13c9e723a67
Michael Robinson <mikelrob@gmail.com>
2014-01-24 23:16:30 +0000

    added GameObject and child classes
    GameView has game loop on timer and a key listener

--- src/colourtastic/Colourtastic.java
+++ src/colourtastic/Colourtastic.java
@@ -4,11 +4,13 @@
  */
 package colourtastic;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author mikelrob
  */
-public class Colourtastic {
+public class Colourtastic extends JFrame{
 
     /**
      * @param args the command line arguments
@@ -16,5 +18,22 @@ public class Colourtastic {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Colourtastic! A game of perception");
+        
+        //start the game
+        Colourtastic game = new Colourtastic();
+    }
+    
+    public Colourtastic(){
+        //constructor
+        setTitle("Colourtastic");
+        setSize(800, 600);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        GameView gameView = new GameView();
+        this.add(gameView);
+        
+        setVisible(true);
     }
 }
--- src/colourtastic/GameView.java
+++ src/colourtastic/GameView.java
@@ -1,38 +1,82 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package colourtastic;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.Timer;
 import javax.swing.JPanel;
 
 /**
- *
+ * GameView class represents the game.
  * @author mikelrob
  */
-public class GameView extends JPanel implements ActionListener{
+public class GameView extends JPanel implements ActionListener, KeyListener{
     
+    //framerate ivars
     int fps = 24;
     int delay = 1000/fps;
+    
+    //actor ivars
+    Player me;
     
     public GameView(){
         Timer gameTimer = new Timer(delay, this);
         gameTimer.start();
+        setFocusable(true);
+        addKeyListener(this);
+        
+        me = new Player();
     }
     
-    public void paint(Graphics g){
+    /**
+     * This method paints the game scene
+     * @param g
+     */
+    @Override
+    public void paintComponent(Graphics g){
+        //fill back ground
         g.setColor(Color.BLACK);
         g.fillRect(100, 100, 100, 100);
+        
+        //draw player
+        g.fillRect(me.getPositionX(), me.getPositionY(), me.getSizeX(), me.getSizeY());
+    }
+
+    /**
+     * This method is triggered by Actions in the Game.
+     * Including framerate timer, key presses etc
+     * @param ae
+     */
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+//        System.out.println(ae.getID());
+//        System.out.println(System.currentTimeMillis());
+        this.repaint();
     }
 
     @Override
-    public void actionPerformed(ActionEvent ae) {
-//        System.out.println(System.currentTimeMillis());
-        this.repaint();
+    public void keyTyped(KeyEvent ke) {
+//        System.out.println(ke.getKeyChar() + " typed");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+//        System.out.println(ke.getKeyChar() + " pressed");
+        int c = ke.getKeyCode();
+        switch(c){
+            case KeyEvent.VK_A:
+                System.out.println("A");
+                break;
+            default:
+                System.out.println("Key not implemented");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        System.out.println(ke.getKeyChar() + " released");
     }
 }
