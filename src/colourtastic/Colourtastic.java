b1d161645921d57a7863140590c4818077ebe423
Michael Robinson <mikelrob@gmail.com>
2014-01-24 20:06:45 +0000

    init commit

---
+++ src/colourtastic/Colourtastic.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colourtastic;
+
+/**
+ *
+ * @author mikelrob
+ */
+public class Colourtastic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Colourtastic!");
+    }
+}
